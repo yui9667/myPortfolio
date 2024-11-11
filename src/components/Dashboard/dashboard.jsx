83847f5e2aada7c9d738afec1294bbd92bd075fd
Yui Jensen <kyanyui67@gmail.com>
2024-11-11 23:28:52 +0100

    added framer montion and adjust color

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -1,23 +1,25 @@
 import { useState } from 'react';
 import './dashboard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { motion, useViewportScroll, useTransform } from 'framer-motion';
 import { faSun, faMoon, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 const Dashboard = () => {
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
   const [isOpen, setIsOpen] = useState(false);
   const toggleHamburger = () => {
     setIsOpen(!isOpen);
   };
   return (
     <div>
-      <div className='relative right-0 '>
+      {/* <div className='relative right-0 '>
         <img
           src='../src/assets/small-design-up.png'
           alt=''
           className='absolute w-28
           '
         />
-      </div>
+      </div> */}
       <nav className='nav-bar  '>
         <img
           src='../src/assets/logo-yui.png'
@@ -59,26 +61,45 @@ const Dashboard = () => {
           <span className='bar'></span>
         </div>
       </nav>
+      <div className=' flex flex-col items-center justify-center'>
+        <div className='w-full max-w-screen-sm bg-yellow-100 '>
+          <header className=''>
+            <div className='flex flex-col items-center justify-center w-full max-m-1'>
+              <motion.h1
+                className='text-4xl  pr-5 pl-5 pt-20'
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                  type: 'spring',
+                  duration: 1,
+                  delay: '0.8',
+                  stiffness: 130,
+                  ease: 'easeIn',
+                }}
+              >
+                Hello, my name is{' '}
+                <strong className='text-[#FE6A00]'>Yui Jensen</strong>
+              </motion.h1>
 
-      <header className=' mt-20 pt-20 '>
-        <div className='flex flex-col items-center justify-center w-full max-m-1'>
-          <h1 className='text-3xl pr-10 pl-10 z-10'>
-            Hello, my name is{' '}
-            <strong className='text-[#FE6A00]'>Yui Jensen</strong>
-          </h1>
-
-          <div className='flex flex-col mt-20 mb-20'>
-            <a href='' className='mb-5 z-10'>
-              Scroll Down
-            </a>
-            <FontAwesomeIcon
-              icon={faArrowDown}
-              style={{ color: '#4D4415' }}
-              className='z-10'
-            />
-          </div>
-        </div>
-        <img
+              <motion.div
+                className='flex flex-col mt-20 mb-20'
+                style={{ scale }}
+              >
+                <motion.a
+                  href=''
+                  className='mb-5 z-10'
+                  style={{ scaleY: scrollYProgress }}
+                >
+                  Scroll Down
+                </motion.a>
+                <FontAwesomeIcon
+                  icon={faArrowDown}
+                  style={{ color: '#4D4415' }}
+                  className='z-10'
+                />
+              </motion.div>
+            </div>
+            {/* <img
           src='../src/assets/small-design-background1.png'
           alt='yellow-circle background 1'
           className='w-30 absolute animate-float1 '
@@ -89,38 +110,40 @@ const Dashboard = () => {
           alt='yellow-circle background 2'
           className='w-28 absolute animate-float2'
           style={{ top: '60%', right: '0%' }}
-        />
-        <div className=' absolute right-0 bottom-0 '>
+        /> */}
+            {/* <div className=' absolute right-0 bottom-0 '>
           <img
             className=' w-28 '
             src='../src/assets/small-design-down.png'
             alt=''
           />
-        </div>
-      </header>
+        </div> */}
+          </header>
 
-      <section className='flex'>
-        <div>
-          <img src='../src/assets/image2.png' alt='' className='pt-20' />
-          <div>
-            <h5 className=''>
-              I am Junior UX designer & Front End Developer with Back End
-              Development knowledge
-            </h5>
-            <a href='' className='underline decoration-1 tracking-tight'>
-              See More About Me
-            </a>
+          <section className='flex'>
             <div>
-              <button>
-                <a href=''>Resume</a>
-              </button>
-              <button>
-                <a href=''>Linkedin</a>
-              </button>
+              <img src='../src/assets/image2.png' alt='' className='pt-20' />
+              <div>
+                <h5 className=''>
+                  I am Junior UX designer & Front End Developer with Back End
+                  Development knowledge
+                </h5>
+                <a href='' className='underline decoration-1 tracking-tight'>
+                  See More About Me
+                </a>
+                <div>
+                  <button>
+                    <a href=''>Resume</a>
+                  </button>
+                  <button>
+                    <a href=''>Linkedin</a>
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
+          </section>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
